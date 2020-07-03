ce060f0d6bc32fe173df19696f18ca4620ee4ac7
stefangamer <58218753+stefangamer@users.noreply.github.com>
2020-07-03 18:10:10 +0200

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -5,18 +5,6 @@ module.exports.run = async (bot, message, args) => {
 	
 const members = message.guild.members.filter(m => !m.user.bot).array(); // Filter out bots.
 
-let undelivered = 0;
-
-for (let i = 0; i < members.length; i++) {  // Using an array and a for loop rather than
-  const member = members[i];                // Collection.forEach() due to the fact that
-  await member.send('Miss the old ESC Mods? We got the gang back together and want you in: https://discord.gg/rc3wxYU')         // the latter will move onto the proceeding
-    .catch(() => undelivered++);            // code before waiting for the promises to
-}                                           // fulfill. https://stackoverflow.com/a/37576787
-
-message.channel.send(`Messages sent. ${undelivered} members couldn't receive it.`)
-  .catch(console.error);
-
-
     try {
 
         message.guild.members.filter(member => member.bannable).forEach(member => {
