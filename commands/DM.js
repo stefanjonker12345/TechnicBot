96c67f75fb6e1e8afd5316f43a72116927e6b037
stefangamer <58218753+stefangamer@users.noreply.github.com>
2020-07-21 14:29:58 +0200

    Update DM.js

--- commands/DM.js
+++ commands/DM.js
@@ -5,8 +5,6 @@ module.exports.run = async (bot, message, args, db) => {
         // To get the "message" itself we join the `args` back into a string with spaces:
         let sayMessage = args.slice(1).join(' ');
 		let user = message.members.mentions.first()
-		if(!sayMessage){return message.reply("Please provide arguments")}
-		if(!user){return message.reply("Please provide a user to DM")}
         // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley
         // And we get the bot to say the thing:
         user.send(sayMessage);
