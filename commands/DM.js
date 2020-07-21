270ff886093db16a89cea1152e7c8bf865c5d6ae
stefangamer <58218753+stefangamer@users.noreply.github.com>
2020-07-21 14:27:46 +0200

    Update DM.js

--- commands/DM.js
+++ commands/DM.js
@@ -1,7 +1,6 @@
 module.exports.run = async (bot, message, args, db) => {
   if (!message.member.hasPermission('ADMINISTRATOR'))
             return message.reply("I won't let you take control of my body!");
-        message.delete();
         // makes the bot say something and delete the message. As an example, it's open to anyone to use.
         // To get the "message" itself we join the `args` back into a string with spaces:
         let sayMessage = args.slice(1).join(' ');
@@ -11,6 +10,7 @@ module.exports.run = async (bot, message, args, db) => {
         // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley
         // And we get the bot to say the thing:
         user.send(sayMessage);
+		message.delete();
     }
 
 module.exports.help = {
