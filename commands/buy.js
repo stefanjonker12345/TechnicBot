f5a67f0a09872ea41c04b9897e7f8ebd28440253
stefangamer <58218753+stefangamer@users.noreply.github.com>
2020-07-24 14:09:09 +0200

    Update buy.js

--- commands/buy.js
+++ commands/buy.js
@@ -3,28 +3,55 @@ let oldcoins = await db.collection('coins').doc(message.member.id.toString()).ge
     return doc.data().balance
 })
 let oldcoinsnum = parseInt(oldcoins)
-let item = args[0]
-let amount = 0;
-let rank = "NaN"
-if (item == "Donator") {
-amount = 100
-rank = "Donator"
-} else if (item == "Ultimate") {
-amount = 150
-rank = "Donator Ultimate"
+let ID = args[0]
+let name = "nill"
+let q = await db.collection('sales').doc(I).get(ID);
+        if (q._fieldsProto[ID]) {
+        name = q.data().name
 } else {
- return message.reply ("Item not found.")
+		return message.reply ("ID not found or already sold")
 }
+let amount = await db.collection('sales').doc(ID).get().then(function(doc) {
+    return doc.data().Price
+})
 let amount2 = parseInt(amount)
 let newamount = oldcoinsnum - amount
 if (newamount < 0) return message.reply("You're too broke pal.")
 db.collection('coins').doc(message.member.id).update({
 balance: newamount
 });
-let memberRole = message.guild.roles.find("name", `${rank}`)
-let member = message.member
-member.addRole(memberRole)
-message.channel.send(`${item} bought for ` + "`" + `${amount}` + "`" + " coins")
+message.channel.send(`Bought` + "`" + `#${ID}` + "`" + `${name} for ${amount}`)
+
+let user = message.member.user.tag
+let userid = message.member.id
+let staffrole = message.guild.roles.find("name", "Eevee");
+let helpmessage = args.join(" ");
+if (!helpmessage) return message.reply ("Please provide a reason!")
+if (!staffrole) return message.reply("No staff role found!")
+let staffid = staffrole.id
+const channel = await message.guild.createChannel(`${user}`, {
+    type: 'text',
+    permissionOverwrites: [
+        {
+            id: message.guild.id,
+            deny: ['VIEW_CHANNEL'],
+        },
+        {
+            id: message.author.id,
+            allow: ['VIEW_CHANNEL', 'SEND_MESSAGES'],
+        },
+        {
+            id: `${staffid}`,
+            allow: ['VIEW_CHANNEL', 'SEND_MESSAGES'],
+        },
+    ],
+})
+const category = message.guild.channels.find(c => c.name == "Sales" && c.type == "category");
+if (!category) return message.reply("Category not found!")
+channel.setParent(category.id);
+
+message.reply("You have been given a personal channel! Please be patient and wait for a Staff member to help you out!")
+channel.send(`${staffrole} ${message.member} **Needs help** with **${helpmessage}**`) 
 }
 
 
