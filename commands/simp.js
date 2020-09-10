cc9333db60d2bfa38278b8fa63d196d4750fbf3b
stefangamer <58218753+stefangamer@users.noreply.github.com>
2020-09-10 10:54:19 +0200

    Update simp.js

--- commands/simp.js
+++ commands/simp.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, message, args, db) => {
 let member = message.mentions.members.first()
 if(!member) return message.reply("you better provide someone who's a simp smh...")
-message.channel.send(`${member}, you are a simp.)
+message.channel.send(`${member}, you are a simp.`)
 }
 
 module.exports.help = {
