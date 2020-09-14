14f9dfeefc8d24193af54579098daaf8d287726f
stefangamer <58218753+stefangamer@users.noreply.github.com>
2020-09-14 09:07:01 +0200

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -2,13 +2,18 @@ module.exports.run = async (bot, message, args, db) => {
 message.delete()
 let reason = args.slice(1).join(' ');
 let warnmember = message.mentions.members.first()
+if(!reason) return message.reply("Please provide a reason!")
+if(!warnmember) return message.reply("Please provide a user to warn!")
 let warns = await db.collection('members').doc(warnmember.id.toString()).get().then(function(doc) {
     return doc.data().warns
 })
 message.channel.send(`${warns}`)
 let newwarns = warns + 1
-db.collection('members').doc(warnmember.id.toString()).update({
+db.collection('members').doc(warnmember.id.toString()).update({	
 [newwarns]: reason
+});
+db.collection('members').doc(warnmember.id.toString()).update({	
+[warns]: newwarns
 });
 }
 
