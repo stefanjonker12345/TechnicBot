48b618046509d40c219c6cfe872954bd7702b53e
stefangamer <58218753+stefangamer@users.noreply.github.com>
2020-11-02 23:26:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ bot.on("ready", () => {
     console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`);
     // Example of changing the bot's playing game to something useful. `client.user` is what the
     // docs refer to as the "ClientUser".
+    bot.user.setStatus(`idle`)
     bot.user.setActivity(`Test`);
 });
 
