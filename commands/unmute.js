126eab7871ea1b6c5b7ab3ac839f3d4483c01302
stefangamer <58218753+stefangamer@users.noreply.github.com>
2020-11-02 23:17:07 +0100

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -2,8 +2,8 @@ module.exports.run = async (bot, message, args, db) => {
 		message.delete();
         let muteduser = message.mentions.members.first();
         if (!muteduser) return message.channel.send("Please specify a user to unmute. For correct usage use `!help unmute`");
-        let role = message.guild.roles.find("name", "muted");
-        muteduser.removeRole(role);
+        let rolee = message.guild.roles.find("name", "muted");
+        muteduser.removeRole(rolee);
         message.channel.send(`Successfully unmuted ${muteduser.user.tag}`)
 }
 
