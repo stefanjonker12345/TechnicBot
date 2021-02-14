ec53264c1a704de2c76dae2c559733c07dac3e68
stefangamer <58218753+stefangamer@users.noreply.github.com>
2021-02-14 17:38:18 +0100

    Update open.js

--- commands/open.js
+++ commands/open.js
@@ -1,6 +1,6 @@
 module.exports.run = async (bot, message, args, db) => {
 message.delete()
-	if (!message.member.roles.some(role => role.name === ["Gym Leader"])) return message.reply("I'm afraid you can't use this!")
+	if (!message.member.roles.some(role => role.name === "GymLeader")) return message.reply("I'm afraid you can't use this!")
 	message.guild.channels
 	let openedchannel = message.channel
 	bot.channels.get("810541518141653022").send(`test`);
