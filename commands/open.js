f6de8eff337202f9f50974b1ce04cd0e8081cb4e
stefangamer <58218753+stefangamer@users.noreply.github.com>
2021-02-14 19:05:21 +0100

    Update open.js

--- commands/open.js
+++ commands/open.js
@@ -3,7 +3,7 @@ message.delete()
 	if (!message.member.hasPermission('MANAGE_NICKNAMES')) return message.reply("I'm afraid you can't do this!")
 	let openedchannel = message.channel
 	bot.channels.get("810541518141653022").send(`${openedchannel} is now open!`);
-	openedchannel.overwritePermissions('807918568862908417', { SEND_MESSAGES: true, ADD_REACTIONS: false});
+	openedchannel.overwritePermissions('807918569090187286', { SEND_MESSAGES: true});
 }
 
 module.exports.help = {
