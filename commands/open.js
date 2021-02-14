f12fa6c83714a9c20bd9d44ee1ad7b4731fded63
stefangamer <58218753+stefangamer@users.noreply.github.com>
2021-02-14 18:51:49 +0100

    Update open.js

--- commands/open.js
+++ commands/open.js
@@ -3,12 +3,12 @@ message.delete()
 	if (!message.member.hasPermission('MANAGE_NICKNAMES')) return message.reply("I'm afraid you can't do this!")
 	let openedchannel = message.channel
 	bot.channels.get("810541518141653022").send(`${openedchannel} is now open!`);
-	openedchannel.overwritePermissions(UserResolvable, { // Pass 'UserResolvable' type thing as described in Wiki!
-  VIEW_CHANNEL: true,
-  SEND_MESSAGES: false,
-  READ_MESSAGE_HISTORY: true,
-  ATTACH_FILES: false
-});
+	openedchannel.overwritePermissions([
+  {
+    id: message.guild.id,
+    allow: ["VIEW_MESSAGES", "SEND_MESSAGES"]
+  }
+]);
 }
 
 module.exports.help = {
