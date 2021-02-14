1c96f81f60421c103fa46d062bb939d5e92b9fed
stefangamer <58218753+stefangamer@users.noreply.github.com>
2021-02-14 17:03:49 +0100

    Create open.js

---
+++ commands/open.js
@@ -0,0 +1,11 @@
+module.exports.run = async (bot, message, args, db) => {
+message.delete()
+	if (!roled.roles.some(role => role.name === "Gym Leader")) return message.reply("I'm afraid you can't use this!")
+	message.guild.channels
+	let openedchannel = message.channel
+	bot.channels.get('810541518141653022').send(`${openedchannel} **just opened**`);
+}
+
+module.exports.help = {
+    name: "open"
+}
