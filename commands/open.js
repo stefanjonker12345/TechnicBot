7f8f527e42f21c5d897100f69d8d02ba14584c49
stefangamer <58218753+stefangamer@users.noreply.github.com>
2021-02-14 18:40:50 +0100

    Update open.js

--- commands/open.js
+++ commands/open.js
@@ -1,6 +1,6 @@
 module.exports.run = async (bot, message, args, db) => {
 message.delete()
-	if (!message.member.hasPermission('MANAGE_NICKNAMES')){
+	if (!message.member.hasPermission('MANAGE_NICKNAMES')) return message.reply("I'm afraid you can't do this!")
 	message.guild.channels
 	let openedchannel = message.channel
 	bot.channels.get("810541518141653022").send(`${openedchannel} is now open!`);
