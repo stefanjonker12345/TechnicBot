39d429e8fdc29e1eb1a2f6f37b8baa7ad5b81229
stefangamer <58218753+stefangamer@users.noreply.github.com>
2021-02-14 19:28:18 +0100

    Update open.js

--- commands/open.js
+++ commands/open.js
@@ -2,7 +2,7 @@ module.exports.run = async (bot, message, args, db) => {
 message.delete()
 	if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply("I'm afraid you can't do this!")
 	let openedchannel = message.channel
-	bot.channels.get("810541518141653022").send(`${openedchannel} is now open!`);
+	bot.channels.get("810541518141653022").send(`${openedchannel} is now open <@&810577930856955964>`);
 	openedchannel.overwritePermissions('807918569090187286', { SEND_MESSAGES: true});
 	message.channel.send("This gym is now opened! Ping the gym leader to challenge this gym!")
 }
