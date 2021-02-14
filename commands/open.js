accaf35c2ceff0d000270228ab254ec8fa1daec3
stefangamer <58218753+stefangamer@users.noreply.github.com>
2021-02-14 17:39:39 +0100

    Update open.js

--- commands/open.js
+++ commands/open.js
@@ -3,7 +3,7 @@ message.delete()
 	if (!message.member.roles.some(role => role.name === "GymLeader")) return message.reply("I'm afraid you can't use this!")
 	message.guild.channels
 	let openedchannel = message.channel
-	bot.channels.get("810541518141653022").send(`test`);
+	bot.channels.get("810541518141653022").send(`${openedchannel} is now open!`);
 }
 
 module.exports.help = {
