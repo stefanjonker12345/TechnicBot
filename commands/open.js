cbe6c4ebc1230f2fd647d263ec5a01185dfafd9c
stefangamer <58218753+stefangamer@users.noreply.github.com>
2021-02-14 17:57:29 +0100

    Update open.js

--- commands/open.js
+++ commands/open.js
@@ -1,6 +1,6 @@
 module.exports.run = async (bot, message, args, db) => {
 message.delete()
-	if (!message.member.roles.some(role => role.name === "GymLeader")) return message.reply("I'm afraid you can't use this!")
+	if(!message.member.roles.some(r=>["Gym Leader", "Oogabooga"].includes(r.name)) )
 	message.guild.channels
 	let openedchannel = message.channel
 	bot.channels.get("810541518141653022").send(`${openedchannel} is now open!`);
