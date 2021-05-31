a0b93fc40ec25d96059e7493ecf1fbbbe1c3bf85
stefangamer <58218753+stefangamer@users.noreply.github.com>
2021-05-31 21:26:53 +0200

    e
    e

---
+++ commands/button.js
@@ -0,0 +1,14 @@
+module.exports.run = async (bot, message, args, db) => {
+message.delete()
+let button = new disbut.MessageButton()
+  .setStyle('red') //default: blurple
+  .setLabel('My First Button!') //default: NO_LABEL_PROVIDED
+  .setID('click_to_function') //note: if you use the style "url" you must provide url using .setURL('https://example.com')
+  .setDisabled(); //disables the button | default: false
+
+message.channel.send('Hey, i am powered by https://npmjs.com/discord-buttons', button);
+}
+
+module.exports.help = {
+    name: "ping"
+}
