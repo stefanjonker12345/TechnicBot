b33e71fe4de0f4f8d84a6e02f68295bbe52174cb
stefangamer <58218753+stefangamer@users.noreply.github.com>
2021-05-31 21:37:26 +0200

    Update button.js

--- commands/button.js
+++ commands/button.js
@@ -1,14 +1,16 @@
 const { MessageButton } = require('discord-buttons');
 
-module.exports = {
-    name: 'button',
-    run: async (client, message, args) => {
-        let button = new disbut.MessageButton()
-          .setStyle('red') //default: blurple
-          .setLabel('My First Button!') //default: NO_LABEL_PROVIDED
-          .setID('click_to_function') //note: if you use the style "url" you must provide url using .setURL('https://example.com')
-          .setDisabled(); //disables the button | default: false
+module.exports.run = async (bot, message, args, db, disbut) => {
+message.delete()
+let button = new disbut.MessageButton()
+  .setStyle('red') //default: blurple
+  .setLabel('My First Button!') //default: NO_LABEL_PROVIDED
+  .setID('click_to_function') //note: if you use the style "url" you must provide url using .setURL('https://example.com')
+  .setDisabled(); //disables the button | default: false
 
-        await message.channel.send('Hey, i am powered by https://npmjs.com/discord-buttons', button);
-    }
+message.channel.send('Hey, i am powered by https://npmjs.com/discord-buttons', button);
+}
+
+module.exports.help = {
+    name: "button"
 }
