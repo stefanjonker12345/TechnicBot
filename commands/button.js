4bf9ed2c4641e603d9452cbcfcb11f09d57f38fd
stefangamer <58218753+stefangamer@users.noreply.github.com>
2021-05-31 21:33:08 +0200

    ee

--- commands/button.js
+++ commands/button.js
@@ -6,7 +6,7 @@ let button = new disbut.MessageButton()
   .setID('click_to_function') //note: if you use the style "url" you must provide url using .setURL('https://example.com')
   .setDisabled(); //disables the button | default: false
 
-message.channel.send('Hey, i am powered by https://npmjs.com/discord-buttons', button);
+await message.channel.send('Hey, i am powered by https://npmjs.com/discord-buttons', button);
 }
 
 module.exports.help = {
