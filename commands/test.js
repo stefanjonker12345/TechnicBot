f6494280a51584e0816bd3b4d9072335881e122a
stefangamer <58218753+stefangamer@users.noreply.github.com>
2021-06-04 21:09:04 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,10 +1,25 @@
 module.exports.run = async (bot, message, args, db) => {
-        message.author.send("See or Change?");
-        const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
-        console.log(collector)
-        collector.on('collect', message => {
-            message.channel.send(`${message.content}`)
+let filter = m => m.author.id === message.author.id
+    message.channel.send(`Are you sure to delete all data? \`YES\` / \`NO\``).then(() => {
+      message.channel.awaitMessages(filter, {
+          max: 1,
+          time: 30000,
+          errors: ['time']
         })
+        .then(message => {
+          message = message.first()
+          if (message.content.toUpperCase() == 'YES' || message.content.toUpperCase() == 'Y') {
+            message.channel.send(`Deleted`)
+          } else if (message.content.toUpperCase() == 'NO' || message.content.toUpperCase() == 'N') {
+            message.channel.send(`Terminated`)
+          } else {
+            message.channel.send(`Terminated: Invalid Response`)
+          }
+        })
+        .catch(collected => {
+            message.channel.send('Timeout');
+        });
+    })
 }
 
 module.exports.help = {
