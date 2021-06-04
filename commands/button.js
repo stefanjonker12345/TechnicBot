ad85efb5018740cbd0dd1bb2bc61a7b1e573e558
stefangamer <58218753+stefangamer@users.noreply.github.com>
2021-06-04 13:13:18 +0200

    Update button.js

--- commands/button.js
+++ commands/button.js
@@ -2,8 +2,9 @@ module.exports.run = async (bot, message, args, db, disbut, MessageButton) => {
  let btn = new bot.disbut.MessageButton()
     .setStyle('red')
     .setLabel('AMONGUS !!! SO SUS !!!!')
-    .setID('amogus');
-	.setDisabled()
+    .setID('amogus')
+	.setDisabled();
+	
   let msg = await message.channel.send('Click for AMOGUS 😳', {
     button: btn
   });
