1b458963627fe1b6df94bf14e1379b3f6d103777
stefangamer <58218753+stefangamer@users.noreply.github.com>
2021-06-04 13:59:26 +0200

    Update button2.js

--- commands/button2.js
+++ commands/button2.js
@@ -1,12 +1,11 @@
 module.exports.run = async (bot, message, args, db) => {
-message.delete()
-let button = new disbut.MessageButton()
-  .setStyle('red') //default: blurple
-  .setLabel('My First Button!') //default: NO_LABEL_PROVIDED
-  .setID('click_to_function') //note: if you use the style "url" you must provide url using .setURL('https://example.com')
-  .setDisabled(); //disables the button | default: false
+let m = await message.channel.send('Click!', button);
 
-await message.channel.send('Hey, i am powered by https://npmjs.com/discord-buttons', button);
+const filter = (button) => button.clicker.user.id === message.author.id;
+const collector = m.createButtonCollector(filter, { time: 5000 }); //collector for 5 seconds
+
+collector.on('collect', b => console.log(`Collected button with the id ${b.id}`));
+collector.on('end', collected => console.log(`Collected ${collected.size} items`));
 }
 
 module.exports.help = {
