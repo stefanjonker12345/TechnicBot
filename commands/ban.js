253ccac45fccba77044cee1702c096b6e3b4f9d2
stefangamer <stefan_jonker@hotmail.nl>
2022-05-25 15:34:17 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,14 +1,14 @@
 module.exports.run = async (Discord, bot, message, args, db) => {
-  if (msg.member.hasPermission("BAN_MEMBERS")) {
-    if (msg.members.mentions.first()) {
+  if (message.member.hasPermission("BAN_MEMBERS")) {
+    if (message.members.mentions.first()) {
         try {
-            msg.members.mentions.first().ban();
+            message.members.mentions.first().ban();
             message.channel.send(`${message.author}, banned ${message.members.mentions.first()}`)
         } catch {
-            msg.reply("I do not have permissions to ban" + msg.members.mentions.first());
+            msg.reply("I do not have permissions to ban" + message.members.mentions.first());
         }
     } else {
-        msg.reply("You do not have permissions to ban" + msg.members.mentions.first());
+        msg.reply("You do not have permissions to ban" + message.members.mentions.first());
     }
 }
 }
