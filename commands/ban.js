f5a275fb7d5820a19745fb955f364f007f9cff9f
stefangamer <stefan_jonker@hotmail.nl>
2022-05-25 15:30:36 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,35 +1,16 @@
 module.exports.run = async (Discord, bot, message, args, db) => {
-  if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can\'t use that!')
-  if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have the right permissions.')
-
-  const member = message.mentions.members.first() 
-
-  if(!args[0]) return message.channel.send('Please specify a user');
-
-  if(!member) return message.channel.send('Can\'t seem to find this user. Sorry \'bout that :/');
-  if(!member.bannable) return message.channel.send('This user can\'t be banned. It is either because they are a mod/admin, or their highest role is higher than mine');
-
-  if(member.id === message.author.id) return message.channel.send('Bruh, you can\'t ban yourself!');
-
-  let reason = args.slice(1).join(" ");
-
-  if(!reason) reason = 'Unspecified';
-
-  member.ban(`${reason}`).catch(err => { 
-    message.channel.send('Something went wrong')
-      console.log(err)
-  })
-
-  const banembed = new Discord.MessageEmbed()
-  .setTitle('Member Banned')
-  .setThumbnail(member.user.displayAvatarURL())
-  .addField('User Banned', member)
-  .addField('Kicked by', message.author)
-  .addField('Reason', reason)
-  .setFooter('Time kicked', client.user.displayAvatarURL())
-  .setTimestamp()
-
-  message.channel.send(banembed);
+  if (msg.member.hasPermission("BAN_MEMBERS")) {
+    if (msg.members.mentions.first()) {
+        try {
+            msg.members.mentions.first().ban();
+            message.channel.send(`${message.author}, banned ${message.members.mentions.first()}`)
+        } catch {
+            msg.reply("I do not have permissions to ban" + msg.members.mentions.first());
+        }
+    } else {
+        msg.reply("You do not have permissions to ban" + msg.members.mentions.first());
+    }
+}
 }
 
 module.exports.help = {
